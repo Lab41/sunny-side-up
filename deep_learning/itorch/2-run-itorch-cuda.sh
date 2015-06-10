d7561b58df1cd87d13a09436ce10f07dfaa81a79
kylef-lab41 <kylef@lab41.org>
2015-06-10 11:11:51 -0700

    Update syntax of itorch run script

--- deep_learning/itorch/2-run-itorch-cuda.sh
+++ deep_learning/itorch/2-run-itorch-cuda.sh
@@ -3,15 +3,17 @@
 # image name
 __ip=<IP-OF-MACHINE>
 __image=lab41/itorch
-__ip_host=80
-__ip_cntr=8888
+__port_host=80
+__port_cntr=8888
+__volume_host=/opt/sunny-side-up
+__volume_cntr=/data
 
 # run image
 docker run  -d \
-            --publish=$__ip_host:$__ip_cntr \
+            --publish=$__port_host:$__port_cntr \
             --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm \
             --device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidia1:/dev/nvidia1 \
-            --volume=$(pwd)/tutorials:/data \
+            --volume=$__volume_host:$__volume_cntr \
             $__image
 
 echo "Visit iTorch notebook at http://$__ip:$__ip_host"
