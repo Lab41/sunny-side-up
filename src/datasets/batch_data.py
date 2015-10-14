b46b3cbe2acf5dc8f56a490bfb559a14df3c342f
pcallier <pcallier@iqt.org>
2015-10-14 20:00:25 +0000

    Interface for iterating over minibatches of data

---
+++ src/datasets/batch_data.py
@@ -0,0 +1,41 @@
+import logging
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+import numpy as np
+
+import amazon_reviews
+import data_utils
+
+
+def batch_data(data_loader, batch_size=128, normalizer_fun=data_utils.normalize, 
+               transformer_fun=data_utils.to_one_hot, flatten=True):
+    '''
+    Batches data, doing all necessary preprocessing and
+    normalization.
+    '''
+
+    docs = []
+    labels = []
+
+    for doc_text, label in data_loader:
+        try:
+            doc_text = normalizer_fun(doc_text)
+            # transform document into a numpy array
+            transformed_doc = transformer_fun(doc_text)
+            docs.append(transformed_doc)
+            labels.append(label)
+        except data_utils.DataException as e:
+            logger.info(e)
+            continue
+        if len(docs) >= batch_size:
+            docs_np = np.array(docs)
+            if flatten==True:
+                # transform to form (batch_size, w*h); flattening doc
+                docs_np = docs_np.reshape(docs_np.shape[0],-1)
+            # labels come out in a separate (batch_size, 1) np array
+            labels_np = np.array(labels).reshape(1, -1)
+            docs = []
+            labels = []
+
+            yield docs_np, labels_np
