82eef7f0be73a3f9ef10b37d05cc890a62d90a7f
pcallier <pcallier@iqt.org>
2015-10-26 22:36:41 +0000

    Full (?), working (?) implementation of Crepe CNN for document classification

--- examples/test_neon.py
+++ examples/test_neon.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.dirname(current_path))
 import logging
 logging.basicConfig()
 logger=logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 import neon
 import neon.layers
@@ -24,29 +24,43 @@ from neon.callbacks.callbacks import Callbacks, Callback
 from neon.backends import gen_backend
 
 import src.datasets.amazon_reviews as amazon
+import src.datasets.data_utils as data_utils
 from src.datasets.batch_data import batch_data, split_data
 from src.datasets.data_utils import from_one_hot
 from src.datasets.neon_iterator import DiskDataIterator
 
-def get_data(batch_size):
-    amz_data = batch_data(amazon.load_data("/root/data/amazon/test_amazon.json.gz"),
-        batch_size=batch_size)
+def get_data(batch_size, doclength=300):
+    # batch data and split it to HDF5
+    # the generators obtained here are not 
+    # used except for debugging
+    amz_data = batch_data(amazon.load_data("/root/data/amazon/reviews_Home_and_Kitchen.json.gz"),
+        batch_size=batch_size,
+        normalizer_fun=lambda x: data_utils.normalize(x, max_length=doclength),
+        transformer_fun=None)
+    h5_repo = "/root/data/pcallier/amazon/home_kitch_split.hd5"
+    (a, b), (a_size, b_size) = split_data(amz_data, 
+                      in_memory=False, 
+                      h5_path=h5_repo,
+                      overwrite_previous=True)
+    logger.debug("Test iteration (shape of one record): {}".format(a.next()[0].shape))
+    logger.debug("Test iteration (one record): {}".format(a.next()[0]))
 
-    h5_repo = "/root/data/pcallier/amazon/demo_split.h5"
-    (a, b), (a_size, b_size) = split_data(amz_data, in_memory=False, 
-                      h5_path=h5_repo)
-    logger.debug("Test iteration: {}".format(a.next()[0].shape))
-    logger.debug("Test iteration: {}".format(a.next()[0]))
-
+    # define functions for use with neon_iterator
+    # TODO: create proper iterator classes and use those
+    # these functions are returend, alongside the batch sizes
     def a_batcher():
         (a,b),(a_size,b_size)=split_data(None, h5_path=h5_repo, overwrite_previous=False)
-        return batch_data(a, normalizer_fun=lambda x: x,
-            transformer_fun=lambda x: None, flatten=False,
+        return batch_data(a, 
+            normalizer_fun=lambda x: x,
+            transformer_fun=lambda x: data_utils.to_one_hot(x[0]), 
+            flatten=True,
             batch_size=batch_size)
     def b_batcher():
         (a,b),(a_size,b_size)=split_data(None, h5_path=h5_repo, overwrite_previous=False)
-        return batch_data(b, normalizer_fun=lambda x: x, 
-            transformer_fun=lambda x: x, flatten=False,
+        return batch_data(b, 
+            normalizer_fun=lambda x: x, 
+            transformer_fun=lambda x: data_utils.to_one_hot(x[0]), 
+            flatten=True,
             batch_size=batch_size)
 
     return (a_batcher, b_batcher), (a_size, b_size)
@@ -88,43 +102,53 @@ def simple_model(nvocab=67,
     ]
     return layers
 
-def get_model(nvocab=67,
-          doclength=1014):
+def crepe_model(nvocab=67, nframes=256):
+    init_gaussian = neon.initializers.Gaussian(0, 0.05)
     layers = [
-        neon.layers.Conv((nvocab, 7, 256), batch_norm=True,
-            init=neon.initializers.Uniform(),
+        neon.layers.Conv((nvocab, 7, nframes), 
+            batch_norm=False,
+            init=init_gaussian,
             activation=neon.transforms.Rectlin()),
-        neon.layers.Pooling(3),
+        neon.layers.Pooling((1, 3)),
 
-        neon.layers.Conv((1, 7, 256), batch_norm=True,
-            init=neon.initializers.Uniform(),
+        neon.layers.Conv((1, 7, nframes), 
+            batch_norm=False,
+            init=init_gaussian,
             activation=neon.transforms.Rectlin()),
-        neon.layers.Pooling(3),
+        neon.layers.Pooling((1, 3)),
 
-        neon.layers.Conv((1, 3, 256), batch_norm=True,
-            init=neon.initializers.Uniform(),
+        neon.layers.Conv((1, 3, nframes),
+            batch_norm=False,
+            init=init_gaussian,
             activation=neon.transforms.Rectlin()),
         
-        neon.layers.Conv((1, 3, 256), batch_norm=True,
-            init=neon.initializers.Uniform(),
+        neon.layers.Conv((1, 3, nframes),
+            batch_norm=False,
+            init=init_gaussian,
             activation=neon.transforms.Rectlin()),
             
-        neon.layers.Conv((1, 3, 256), batch_norm=True,
-            init=neon.initializers.Uniform(),
+        neon.layers.Conv((1, 3, nframes), 
+            batch_norm=False,
+            init=init_gaussian,
             activation=neon.transforms.Rectlin()),
 
-        neon.layers.Conv((1, 3, 256), batch_norm=True,
-            init=neon.initializers.Uniform(),
+        neon.layers.Conv((1, 3, nframes), 
+            batch_norm=False,
+            init=init_gaussian,
             activation=neon.transforms.Rectlin()),
-        neon.layers.Pooling(3),
-
-        neon.layers.Affine(1024,
-            init=neon.initializers.Uniform(),
-            activation=neon.transforms.Rectlin()),
+        neon.layers.Pooling((1, 3)),
 
         neon.layers.Affine(1024,
-            init=neon.initializers.Uniform(),
+            init=init_gaussian,
             activation=neon.transforms.Rectlin()),
+
+        neon.layers.Dropout(0.5),
+
+        neon.layers.Affine(1024,
+            init=init_gaussian,
+            activation=neon.transforms.Rectlin()),
+
+        neon.layers.Dropout(0.5),
 
         neon.layers.Affine(2,
             init=neon.initializers.Uniform(),
@@ -155,22 +179,41 @@ class MyCallback(Callback):
 
 def main():
     batch_size=64
-    be = gen_backend(backend='gpu', batch_size=batch_size)
-    (train_get, test_get), (train_size, test_size) = get_data(batch_size)
+    doc_length=1014
+    vocab_size=67
+    gpu_id=0
+    nframes=256
+
+    model_state_path="/data/pcallier/amazon/neon_crepe_model.hd5"
+    logger.info("Getting backend...")
+    be = gen_backend(backend='gpu', batch_size=batch_size, device_id=0)
+    logger.info("Getting data...")
+    (train_get, test_get), (train_size, test_size) = get_data(batch_size, doc_length)
     train_batch_beta = test_get()
     logger.debug("First record shape: {}".format(train_batch_beta.next()[0].shape))
 
-    train_iter = DiskDataIterator(train_get, ndata=train_size)
-    test_iter = DiskDataIterator(test_get, ndata=test_size)
-    #print ''.join(from_one_hot(a.next()[0][0].reshape((-1,1014))))[::-1]
-    model = lstm_model()
-    mlp = neon.models.Model(model)
+    train_iter = DiskDataIterator(train_get, ndata=train_size, 
+                                  doclength=doc_length, 
+                                  nvocab=vocab_size)
+    test_iter = DiskDataIterator(test_get, ndata=test_size, 
+                                  doclength=doc_length, 
+                                  nvocab=vocab_size)
+    logger.info("Building model...")
+    model_layers = crepe_model(nframes=nframes)
+    mlp = neon.models.Model(model_layers)
     cost = neon.layers.GeneralizedCost(neon.transforms.CrossEntropyBinary())
     callbacks = Callbacks(mlp,train_iter,valid_set=test_iter,valid_freq=3,progress_bar=True)
-    my_callback = MyCallback()
+    callbacks.add_save_best_state_callback(model_state_path)
+    callbacks.add_validation_callback(test_iter, 2)
+    optimizer = neon.optimizers.GradientDescentMomentum(
+        learning_rate=0.01,
+        momentum_coef=0.9,
+        schedule=neon.optimizers.Schedule([2,5,8], 0.5))
+    #optimizer = neon.optimizers.Adadelta()
+    # my_callback = MyCallback()
     # callbacks.add_callback(my_callback, 0)
-    logger.debug(callbacks.callbacks)
-    mlp.fit(train_iter, optimizer=neon.optimizers.RMSProp(), 
+    logger.info("Doing training...")
+    mlp.fit(train_iter, optimizer=optimizer, 
               num_epochs=10, cost=cost, callbacks=callbacks)
     print "Misclassification error: {}".format(mlp.eval(test_iter, metric=neon.transforms.Misclassification()))
 
