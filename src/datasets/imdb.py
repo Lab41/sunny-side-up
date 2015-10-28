a03f5076452db86610525272f3f84e9e256ae3cc
pcallier <pcallier@iqt.org>
2015-10-28 20:34:53 +0000

    IMDB loader paramters changed, now better-behaved

--- src/datasets/imdb.py
+++ src/datasets/imdb.py
@@ -1,51 +1,65 @@
+#!/usr/bin/env python
+
 import os
 import tarfile
 import random
+import logging
+logging.basicConfig()
+logger=logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 from data_utils import get_file
 
-
-# Dictionary that defines the Sentiment features
-#Sentiment = {0: 'neg', 2: 'neutral', 4: 'pos'}
 pos_label = 1
 neg_label = 0
 
-def load_data(file_path=None, download_path="./.downloads", dest_path="./.downloads"):
+def load_data(file_dir="./.downloads", download_dir="./.downloads"):
     ''' Function that takes in a path to the IMDB movie review dataset
         word analogy file, opens it, removes topic tags and returns a list
         of the analogies
 
         @Arguments:
-            file_path -- (optional) personal system file path to the
-                IMDB data set in gzip form(or others of
-                a similar structure)
+            file_dir -- personal system file path to the
+                unzipped IMDB data set (so, a directory). If this does
+                not exist, the archive will be downloaded and unzipped here
+            download_dir -- what directory to download the actual archive to? Can be None,
+                in which case it defaults to the parent directory of file_path.
+                The archive will only be downloaded if necessary
 
         @Return:
             A generator over a tuples of Movie reviews and their sentiment
     '''
     # Open file path
-    if not file_path:
-        print "Downloading IMDB dataset"
-        file_path = get_file("http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz",
-                             download_path)
+    if not os.path.isdir(file_dir):
+        logging.info("Downloading IMDB dataset")
+        if download_dir is None:
+            download_dir = os.path.dirname(os.path.normpath(file_dir))
+        downloaded_file_path = get_file("http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz",
+                             download_dir)
+        # then extract it 
+        if not os.path.isdir(os.path.join(file_dir, 'aclImdb')):
+            logging.info("Extracting IMDB dataset")
+            tar = tarfile.open(downloaded_file_path, mode="r:gz")
+            tar.extractall(path=file_dir)
+            tar.close()
 
-    # If file has not been extracted, then extract it 
-    # to the downloads folder. This will save a lot of time
-    if not os.path.isdir(os.path.join(dest_path, 'aclImdb')):
-        print("Extracting IMDB dataset")
-        tar = tarfile.open(file_path, mode="r:gz")
-        tar.extractall(path=dest_path)
-        tar.close()
-
+    imdb_root = os.path.join(file_dir, "aclImdb")
+    imdb_train = os.path.join(imdb_root, "train")
+    imdb_test = os.path.join(imdb_root, "test")
+    imdb_train_pos = os.path.join(imdb_train, "pos")
+    imdb_train_neg = os.path.join(imdb_train, "neg")
+    imdb_test_pos = os.path.join(imdb_test, "pos")
+    imdb_test_neg = os.path.join(imdb_test, "neg")
+    
     # Specifies positive and negative files
-    pos_train = os.listdir('./.downloads/aclImdb/train/pos')
-    pos_train = [(os.path.join('./.downloads/aclImdb/train/pos', file_name), pos_label) for file_name in pos_train]
-    pos_test = os.listdir('./.downloads/aclImdb/test/pos')
-    pos_test = [(os.path.join('./.downloads/aclImdb/test/pos', file_name), pos_label) for file_name in pos_test]
+    pos_train = os.listdir(imdb_train_pos)
+    pos_train = [(os.path.join(imdb_train_pos, file_name), pos_label) for file_name in pos_train]
+    pos_test = os.listdir(imdb_test_pos)
+    pos_test = [(os.path.join(imdb_test_pos, file_name), pos_label) for file_name in pos_test]
 
-    neg_train = os.listdir('./.downloads/aclImdb/train/neg')
-    neg_train = [(os.path.join('./.downloads/aclImdb/train/neg', file_name), neg_label) for file_name in neg_train]
-    neg_test = os.listdir('./.downloads/aclImdb/test/neg')
-    neg_test = [(os.path.join('./.downloads/aclImdb/test/neg', file_name), neg_label) for file_name in neg_test]
+    neg_train = os.listdir(imdb_train_neg)
+    neg_train = [(os.path.join(imdb_train_neg, file_name), neg_label) for file_name in neg_train]
+    neg_test = os.listdir(imdb_test_neg)
+    neg_test = [(os.path.join(imdb_test_neg, file_name), neg_label) for file_name in neg_test]
 
     all_data = pos_train + pos_test + neg_train + neg_test
 
@@ -58,3 +72,10 @@ def load_data(file_path=None, download_path="./.downloads", dest_path="./.downlo
         yield (f.read(), sentiment)
         # Closes f on the following next() call by user
         f.close()
+
+def main():
+    data = load_data()
+    print data.next()
+
+if __name__=="__main__":
+    main()
