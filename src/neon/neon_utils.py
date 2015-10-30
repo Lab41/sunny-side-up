1db35380b3bec7f6618f38c5cf6cd144f516716d
pcallier <pcallier@iqt.org>
2015-10-30 22:55:14 +0000

    Make confusion matrix work and save its results

--- src/neon/neon_utils.py
+++ src/neon/neon_utils.py
@@ -5,6 +5,12 @@ evaluation of neon models"""
 
 import os
 import json
+import logging
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+import numpy as np
 import neon
 #import matplotlib
 #import sklearn.metrics
@@ -19,6 +25,8 @@ class NeonCallback(neon.callbacks.callbacks.Callback):
             - confusion matrix
     """
     def __init__(self, model, train_data, test_data, save_path):
+        self.conf_matrix_binary = ConfusionMatrixBinary()
+        super(NeonCallback, self).__init__()
         self.model = model
         self.train_data = train_data
         self.test_data = test_data
@@ -30,7 +38,7 @@ class NeonCallback(neon.callbacks.callbacks.Callback):
         self.test_accuracies = []
         self.test_confusions = []
     
-
+    @staticmethod
     def write_to_json(obj, path_base, path_decorator):
         """
         Writes (or overwrites) a JSON file located via a regular 
@@ -54,31 +62,35 @@ class NeonCallback(neon.callbacks.callbacks.Callback):
         new_cost_scalar = float(cost_container.get()[0,0])
         #logger.debug(new_cost_scalar)
         # add to costs structure
-        if epoch > len(self.costs):
+        if epoch >= len(self.costs):
             self.costs.append([])
             logger.debug("Epoch {}, adding to costs list, now len {}".format(epoch,len(self.costs)))
         self.costs[epoch].append(new_cost_scalar)
         # save total cost to placeholder to compute difference in next batch
         self.old_cost[:] = self.model.total_cost
         # serialize every so often
-        if len(self.costs[epoch]) % 100 == 0:
+        if len(self.costs[epoch]) % 500 == 0:
             self.write_to_json(self.costs, self.save_path, "_costs")
 
 
-    def on_epoch_end(self, epoch, epochs):
+    def on_epoch_end(self, epoch):
         """Get train/test accuracy, produce
         epoch-wide charts of loss per minibatch"""
         # get accuracy scores
+        logger.info("Computing training accuracy")
         train_accuracy = self.model.eval(self.train_data, neon.transforms.Accuracy())
-        test_confusion = self.model.eval(self.test_data, ConfusionMatrixBinary())
-        # append and serialize
+        logger.info("Computing testing accuracy")
+        test_accuracy = self.model.eval(self.test_data, neon.transforms.Accuracy())
+        logger.info("Computing confusions")
+        test_confusion = self.conf_matrix_binary.get(self.model, self.test_data)
+        # append and serialize to disk
         self.train_accuracies.append(train_accuracy)
         self.test_accuracies.append(test_accuracy)
         self.test_confusions.append(test_confusion)
         train_test_acc = { 'train': self.train_accuracies,
                            'test' : self.test_accuracies }
         self.write_to_json(train_test_acc, self.save_path, "_accuracies")
-        self.write_to_json(test_confusions, self.save_path, "_confusions")
+        self.write_to_json(self.test_confusions, self.save_path, "_confusions")
         # finish writing costs to disk
         self.write_to_json(self.costs, self.save_path, "_costs")
         # TODO:  plot loss over the epoch
@@ -88,12 +100,16 @@ class NeonCallback(neon.callbacks.callbacks.Callback):
             
 
 class NeonCallbacks(neon.callbacks.callbacks.Callbacks):
-    def add_neon_callback(self, metrics_path):
+    def add_neon_callback(self, metrics_path, **kwargs):
         self.add_callback(NeonCallback(self.model,
                                        self.train_set,
                                        self.valid_set,
-                                       metrics_path))
-                                       
+                                       metrics_path), **kwargs)
+    def __init__(self, model, train_set, output_file=None, valid_set=None,
+                 valid_freq=None, progress_bar=True):
+        super(NeonCallbacks, self).__init__(model, train_set, output_file,
+            valid_set, valid_freq, progress_bar)
+        self.valid_set = valid_set
 
 class ConfusionMatrixBinary(neon.transforms.cost.Metric):
 
@@ -104,29 +120,30 @@ class ConfusionMatrixBinary(neon.transforms.cost.Metric):
     def __init__(self):
         self.preds = self.be.iobuf(1)
         self.hyps = self.be.iobuf(1)
-        self.outputs = self.preds  # Contains per record metric
+        self.outputs = self.be.zeros((2,2))
         self.metric_names = ['ConfusionMatrixBinary']
+        logger.debug("Initting ConfusionMatrixBinary metric")
 
     def __call__(self, y, t):
         """
-        Compute the accuracy metric
+        Compute the confusion matrix metric
 
         Args:
             y (Tensor or OpTree): Output of previous layer or model
             t (Tensor or OpTree): True targets corresponding to y
 
         Returns:
-            numpy.array: Returns the metric
+            dict: Returns the metric
         """
         # convert back from onehot and compare
         self.preds[:] = self.be.argmax(y, axis=0)
         self.hyps[:] = self.be.argmax(t, axis=0)
-        self.outputs[:] = self.be.equal(self.preds, self.hyps)
+        self.matches[:] = self.be.equal(self.preds, self.hyps)
 
         conf_matrix = dict()
-        predictions = self.preds.get()
-        truth = self.hyps.get()
-        matches = self.outputs.get()
+        predictions = self.preds.get().astype(bool)
+        truth = self.hyps.get().astype(bool)
+        matches = self.outputs.get().astype(bool)
         
         # true positives
         conf_matrix['tp'] = np.sum(matches & truth)
@@ -136,6 +153,8 @@ class ConfusionMatrixBinary(neon.transforms.cost.Metric):
         conf_matrix['fp'] = np.sum(np.logical_not(matches) & np.logical_not(truth))
         # false negatives
         conf_matrix['fn'] = np.sum(np.logical_not(matches) & truth)
+
+        self.outputs[:] = np.array([[conf_matrix['tp'], conf_matrix['fp']],[conf_matrix['fn'], conf_matrix['tn']]])
 
         return conf_matrix
 
