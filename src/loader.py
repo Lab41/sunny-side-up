a81a79d3bc30a85a3437fdc3513959f9deb1281f
kylef-lab41 <kylef@lab41.org>
2015-11-02 13:14:58 -0800

    Update sentiment140 data loader

--- src/loader.py
+++ src/loader.py
@@ -20,6 +20,28 @@ import preprocess
 
 cacheDir = "%s/.cached_data" % currentPath
 
+
+# overall labels
+label_positive = 1
+label_neutral = 0
+label_negative = -1
+
+# sentiment140 download parameters
+url_sentiment140 = "http://cs.stanford.edu/people/alecmgo/trainingandtestdata.zip"
+dir_tmp_sentiment140 = '/tmp/sentiment140'
+csv_sentiment140 = 'training.1600000.processed.noemoticon.csv'
+index_sentiment140_label = 0
+index_sentiment140_text = 5
+label_sentiment140_positive = '4'
+label_sentiment140_negative = '0'
+
+map_amazon_label = {  1.0: label_negative,
+                      2.0: label_negative,
+                      3.0: label_neutral,
+                      4.0: label_positive,
+                      5.0: label_positive }
+
+
 # Filled in at run-time
 sizes = {}
 
@@ -50,86 +72,133 @@ def cacheMaker(cachePath):
 def read_sentiment140(sentiment140Path = "/data/sentiment140/sentiment140.csv"):
     """
         Get a generator for the sentiment140 dataset
-          may need to provide local path to data.
+
+        @Arguments:
+            sentiment140Path: path to load (and/or save) file
+
+        Process:
+            Download file if not present
+            Cache file as (tweet,label) pairs
+
+        @Return:
+            generator to cached file
     """
 
     senti140Cache = join(cacheDir, "sentiment140.json")
+
+
+    # create cached file if necessary
     if not exists(senti140Cache):
         ensureCache()
-        if not sentiment140Path or not exists(sentiment140Path):
+
+        # request path to file if necessary
+        if not sentiment140Path:
             print("Please provide the local path to the sentiment140 dataset: ")
             sentiment140Path = sys.stdin.readline().strip()
 
+        # download the file if it doesn't exist
+        if not exists(sentiment140Path):
+
+            # download entire source zipfile from internet
+            print("Downloading sentiment140 dataset from Stanford...")
+            file_path = get_file(url_sentiment140)
+
+            # save specified CSV from zipfile
+            with ZipFile(file_path, 'r') as zp:
+                zp.extract(csv_sentiment140, dir_tmp_sentiment140)
+                shutil.move(os.path.join(dir_tmp_sentiment140, csv_sentiment140), sentiment140Path)
+
+        # write to cache
         with open(senti140Cache,"w") as cacheFile:
             with open(sentiment140Path) as sentiPath:
-                reader = csv.reader(sentiPath)
-                for line in reader:
-                    cacheFile.write( json.dumps([preprocess.tweet(line[5].decode("utf-8")), 1 if line[0] == '4' else -1]) )
+
+                # enumerate over CSV entries
+                reader = latin_csv_reader(sentiPath, delimiter=',')
+                for i, line in enumerate(reader):
+
+                    # format text
+                    text = preprocess.tweet(line[index_sentiment140_label])
+
+                    # generate binary label
+                    if line[index_sentiment140_text] == label_sentiment140_positive:
+                      label = label_positive
+                    else:
+                      label = label_negative
+
+                    # write (text,label) pairs
+                    cacheFile.write( json.dumps([text, label]) )
                     cacheFile.write("\n")
-                    
+
     return cacheMaker(senti140Cache)
-    
+
+
+
+
 def read_amazon( amazonPath = "/data/amazon/aggressive_dedup.json" ):
     # Review producer for amazon
     amazonCache = join(cacheDir, "amazon.json")
-    if not exists( amazonCache ): 
+
+    # generate cached file if necessary
+    if not exists(amazonCache):
         if not amazonPath or not exists(amazonPath):
             print("Please provide the local path to the amazon reviews dataset: ")
             amazonPath = sys.stdin.readline().strip()
 
-        reviewLevel = { 1.0 : -1, 2.0: -1, 3.0: 0, 4.0: 1, 5.0 : 1 }
         ensureCache()
         with open(amazonCache, "w") as cacheFile:
             with open(amazonPath) as afile:
                 for l in afile:
                     jl = json.loads(l)
-                    cacheFile.write( json.dumps([jl["reviewText"], reviewLevel[ jl["overall"] ] ]) )
+                    cacheFile.write( json.dumps([jl["reviewText"], map_amazon_label[ jl["overall"] ] ]) )
                     cacheFile.write("\n")
-        
+
     return cacheMaker(amazonCache)
 
 def read_imdb(imdbPath = "/data/aclImdb/aclImdb_v1.tar"):
-    
+
     imdbCache = join(cacheDir, "imdb.json")
-    
-    polNum = {"pos":1, "neg":-1, "unsup":0}
-    
+
+    polNum = {  "pos":  label_positive,
+                "neg":  label_negative,
+                "unsup":label_neutral }
+
     if not exists(imdbCache):
         if not imdbPath or not exists(imdbPath):
             print("Please provide the local path to the imdb reviews dataset: ")
             imdbPath = sys.stdin.readline().strip()
-            
+
         with tarfile.open(imdbPath) as tarf:
             ensureCache()
             with open(imdbCache,"w") as cacheFile:
                 for name in tarf.getnames():
                     if name.endswith(".txt"):
                         try:
-                            
+
                             pol = name.split("/")[2]
                             pNum = polNum[pol]
                             review = tarf.extractfile(name).read().strip()
                             cacheFile.write( json.dumps([review, pNum]) )
                             cacheFile.write("\n")
-                            
+
                         except Exception as e:
                             print("Ignoring error on %s" % name)
-    
+
     return cacheMaker(imdbCache)
-    
+
 def limiter(baseGen, limit):
     assert(limit > 0)
     while limit > 0:
         limit -= 1
         yield( next(baseGen) )
-            
+
 def sampler(baseGen, sampleRate):
     for item in baseGen:
         if random.random() < sampleRate:
             yield( item )
-    
+
 def read(dataset, dataPath=None, limit=None, sampleRate=None):
-    
+
+    # obtain generator over (tweet,label) pairs
     gen = None
     if dataset == "amazon":
         gen = read_amazon(dataPath)
@@ -140,11 +209,14 @@ def read(dataset, dataPath=None, limit=None, sampleRate=None):
     else:
         print("No dataset, %s, your options are: imdb, amazon or sentiment140" % dataset)
         exit()
-    
+
+    # sample data if specified
     if sampleRate:
         gen = sampler(gen, sampleRate)
-    
+
+    # limit maximum results
     if limit:
         gen = limiter(gen, limit)
-    
+
+    # return generator over data
     return gen
