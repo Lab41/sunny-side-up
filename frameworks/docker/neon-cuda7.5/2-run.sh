e4191c5e9f02d7411ac9df531dd4d163ad919821
pcallier <pcallier@iqt.org>
2015-11-05 23:05:01 +0000

    Add support for either 1 or 2 GPUs

---
+++ frameworks/docker/neon-cuda7.5/2-run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# choose Docker image to use 
+__image=lab41/neon-cuda7.5
+#__image=lab41/neon-cuda
+
+# put together docker cmd with variable number of devices
+CMDSTR="docker run -it --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm "
+# device 0
+if [ -e '/dev/nvidia0' ]; then
+   CMDSTR="${CMDSTR} --device /dev/nvidia0:/dev/nvidia0 "
+fi
+# device 1
+if [ -e '/dev/nvidia1' ]; then
+   CMDSTR="${CMDSTR} --device /dev/nvidia1:/dev/nvidia1 "
+fi
+# mount point, image, and command
+CMDSTR="${CMDSTR} --volume $(pwd):/root/data '$__image' bash"
+
+eval "$CMDSTR"
+
