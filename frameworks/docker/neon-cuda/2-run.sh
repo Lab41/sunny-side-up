73c23e8d93829feb54564df213de2f6352f1c90f
pcallier <pcallier@iqt.org>
2015-11-06 01:46:27 +0000

    Attach 1 or 2 GPUs as available

--- frameworks/docker/neon-cuda/2-run.sh
+++ frameworks/docker/neon-cuda/2-run.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# name Docker imagee 
 __image=lab41/neon-cuda
 
-docker run  -it \
-            --privileged \
-            --volume $(pwd):/root/data \
-            $__image \
-              bash
+# put together docker cmd with variable number of devices
+CMDSTR="docker run -it --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm "
+# device 0
+if [ -e '/dev/nvidia0' ]; then
+   CMDSTR="${CMDSTR} --device /dev/nvidia0:/dev/nvidia0 "
+fi
+# device 1
+if [ -e '/dev/nvidia1' ]; then
+   CMDSTR="${CMDSTR} --device /dev/nvidia1:/dev/nvidia1 "
+fi
+# mount point, image, and command
+CMDSTR="${CMDSTR} --volume $(pwd):/root/data '$__image' bash"
+
+eval "$CMDSTR"
+
