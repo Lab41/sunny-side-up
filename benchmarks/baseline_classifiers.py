40c845be3c327a09a041a9a7af975262b9d98e4f
kylef-lab41 <kylef@lab41.org>
2015-11-09 15:25:36 -0800

    Update baseline benchmarking to iterate datasets and word embeddings

--- benchmarks/baseline_classifiers.py
+++ benchmarks/baseline_classifiers.py
@@ -1,4 +1,4 @@
-import os, logging
+import os, sys, logging
 import json
 import numpy as np
 from collections import defaultdict, Counter
@@ -14,39 +14,37 @@ from sklearn.linear_model import LogisticRegression
 from src.datasets import data_utils
 from src.datasets.data_utils import timed
 from src.datasets.imdb import IMDB
+from src.datasets.sentiment140 import Sentiment140
 from src.datasets.word_vector_embedder import WordVectorEmbedder
 
 data_fraction_test = 0.20
 data_fraction_train = 0.80
-dir_data = "/data"
+
+# setup logging
+logger = data_utils.syslogger(__name__)
 
 # set output directory
+dir_data = "/data"
 try:
     dir_results = os.path.join(dir_data, os.path.dirname(os.path.realpath(__file__)), 'results')
 except NameError:
     dir_results = os.path.join(dir_data, 'results')
 
+# data inputs
+datasets =  {
+                'imdb':         {
+                                    'class':    IMDB,
+                                    'path':     os.path.join(dir_data, 'imdb')
+                                },
+                'sentiment140': {
+                                    'class':    Sentiment140,
+                                    'path':     os.path.join(dir_data, 'sentiment140.csv')
+                                }
+            }
 
-# profiled methods
-@timed
-def timed_training(classifier, values, labels):
-    return classifier.fit(values, labels)
-
-@timed
-def timed_testing(classifier, values):
-    return classifier.predict(values)
-
-@timed
-def timed_dataload(data, values, labels):
-    for i, (text, sentiment) in enumerate(data):
-
-        if (i % int(len(values)/20) == 0):
-            print("Embedding {}...".format(i))
-
-        values[i] = embedder.embed_words_into_vectors_averaged(text)
-        labels[i] = sentiment
-
-
+# word embeddings
+def embedders():
+    return ['glove']
 
 def classifiers():
     """
@@ -69,88 +67,119 @@ def classifiers():
            ("SVM", svm.SVC())]
 
 
-# prepare to embed words into vector space
-embedder_model = 'glove'
-embedder = WordVectorEmbedder(embedder_model)
 
 
-# load datasets
-data_source = "imdb"
-loader = IMDB(os.path.join(dir_data, data_source))
-data = loader.load_data()
+# profiled methods
+@timed
+def timed_training(classifier, values, labels):
+    return classifier.fit(values, labels)
+
+@timed
+def timed_testing(classifier, values):
+    return classifier.predict(values)
+
+@timed
+def timed_dataload(data, values, labels):
+    for i, (text, sentiment) in enumerate(data):
+
+        if (i % int(len(values)/20) == 0):
+            logger.info("Embedding {}...".format(i))
+
+        values[i] = embedder.embed_words_into_vectors_averaged(text)
+        labels[i] = sentiment
 
 
-# load dataset
-values = np.zeros((loader.num_samples(), embedder.num_features()), dtype="float32")
-labels = np.zeros(loader.num_samples(), dtype='float32')
-profile_results = timed_dataload(data, values, labels)
-seconds_loading = profile_results.timer.total_tt
+
+# test all vector models
+for embedder_model in embedders():
+
+    # initialize vector embedder
+    embedder = WordVectorEmbedder(embedder_model)
+
+    # iterate all datasources
+    for data_source, data_params in datasets.iteritems():
+
+        # prepare data loader
+        klass = data_params['class']
+        loader = klass(data_params['path'])
+        data = loader.load_data()
 
 
-# split into training and test data
-labels_train, labels_dev, labels_test = data_utils.split_data(labels, train=data_fraction_train, dev=0, test=data_fraction_test)
-values_train, values_dev, values_test = data_utils.split_data(values, train=data_fraction_train, dev=0, test=data_fraction_test)
+        # load dataset
+        logger.info("loading dataset {}...".format(data_params['path']))
+        values = np.zeros((loader.num_samples(), embedder.num_features()), dtype="float32")
+        labels = np.zeros(loader.num_samples(), dtype='float32')
+        profile_results = timed_dataload(data, values, labels)
+        seconds_loading = profile_results.timer.total_tt
 
 
-# calculate distribution
-dist = Counter()
-dist.update(labels_test)
+        # split into training and test data
+        logger.info("splitting dataset into training and testing sets...")
+        labels_train, labels_dev, labels_test = data_utils.split_data(labels, train=data_fraction_train, dev=0, test=data_fraction_test)
+        values_train, values_dev, values_test = data_utils.split_data(values, train=data_fraction_train, dev=0, test=data_fraction_test)
 
 
-# setup classifier
-for classifier_name,classifier in classifiers():
+        # calculate distribution
+        dist = Counter()
+        dist.update(labels_test)
 
-    # profiled training
-    logging.info("Building %s classifier..." % classifier.__class__.__name__)
-    profile_results = timed_training(classifier, values_train, labels_train)
-    seconds_training = profile_results.timer.total_tt
 
-    # profiled testing
-    profile_results = timed_testing(classifier, values_test)
-    predictions = profile_results.results
-    seconds_testing = profile_results.timer.total_tt
+        # setup classifier
+        for classifier_name,classifier in classifiers():
 
-    # calculate metrics
-    data_size           = len(labels_test)
-    data_positive       = np.sum(labels_test)
-    data_negative       = data_size - data_positive
-    confusion_matrix    = metrics.confusion_matrix(labels_test, predictions)
-    TN                  = confusion_matrix[0][0]
-    FP                  = confusion_matrix[0][1]
-    FN                  = confusion_matrix[1][0]
-    TP                  = confusion_matrix[1][1]
-    accuracy            = metrics.accuracy_score(labels_test, predictions)
-    precision           = metrics.precision_score(labels_test, predictions)
-    recall              = metrics.recall_score(labels_test, predictions)
-    f1                  = metrics.f1_score(labels_test, predictions)
+            # profiled training
+            logger.info("Training %s classifier..." % classifier.__class__.__name__)
+            profile_results = timed_training(classifier, values_train, labels_train)
+            seconds_training = profile_results.timer.total_tt
 
-    # build results object
-    results = { 'classifier':   str(classifier.__class__.__name__),
-                'data':    {    'source': str(data_source),
-                                'testsize': str(data_size),
-                                'positive': str(data_positive),
-                                'negative': str(data_negative),
-                                'time_in_seconds_loading': str(seconds_loading)
-                           },
-                'metrics': {    'TP': str(TP),
-                                'FP': str(FP),
-                                'TN': str(TN),
-                                'FN': str(FN),
-                                'accuracy': str(accuracy),
-                                'precision': str(precision),
-                                'recall': str(recall),
-                                'f1': str(f1),
-                                'time_in_seconds_training': str(seconds_training),
-                                'time_in_seconds_testing': str(seconds_testing)
-                            }
-               }
+            # profiled testing
+            logger.info("Testing %s classifier..." % classifier.__class__.__name__)
+            profile_results = timed_testing(classifier, values_test)
+            predictions = profile_results.results
+            seconds_testing = profile_results.timer.total_tt
 
-    # ensure output directory exists
-    if not os.path.isdir(dir_results):
-        data_utils.mkdir_p(dir_results)
+            # calculate metrics
+            data_size           = len(labels_test)
+            data_positive       = np.sum(labels_test)
+            data_negative       = data_size - data_positive
+            confusion_matrix    = metrics.confusion_matrix(labels_test, predictions)
+            TN                  = confusion_matrix[0][0]
+            FP                  = confusion_matrix[0][1]
+            FN                  = confusion_matrix[1][0]
+            TP                  = confusion_matrix[1][1]
+            accuracy            = metrics.accuracy_score(labels_test, predictions)
+            precision           = metrics.precision_score(labels_test, predictions)
+            recall              = metrics.recall_score(labels_test, predictions)
+            f1                  = metrics.f1_score(labels_test, predictions)
 
-    # save json file
-    filename_results = "{}_{}_{}.json".format(data_source, embedder_model, classifier.__class__.__name__)
-    with open(os.path.join(dir_results,filename_results), 'a') as outfile:
-        json.dump(results, outfile, sort_keys=True, indent=4, separators=(',', ': '))
-        outfile.write('\n')
+            # build results object
+            results = { 'classifier':   str(classifier.__class__.__name__),
+                        'data':    {    'source': str(data_source),
+                                        'testsize': str(data_size),
+                                        'positive': str(data_positive),
+                                        'negative': str(data_negative),
+                                        'time_in_seconds_loading': str(seconds_loading)
+                                   },
+                        'metrics': {    'TP': str(TP),
+                                        'FP': str(FP),
+                                        'TN': str(TN),
+                                        'FN': str(FN),
+                                        'accuracy': str(accuracy),
+                                        'precision': str(precision),
+                                        'recall': str(recall),
+                                        'f1': str(f1),
+                                        'time_in_seconds_training': str(seconds_training),
+                                        'time_in_seconds_testing': str(seconds_testing)
+                                    }
+                       }
+
+            # ensure output directory exists
+            if not os.path.isdir(dir_results):
+                data_utils.mkdir_p(dir_results)
+
+            # save json file
+            filename_results = "{}_{}_{}.json".format(data_source, embedder_model, classifier.__class__.__name__)
+            logger.info("Saving results to {}...".format(filename_results))
+            with open(os.path.join(dir_results,filename_results), 'a') as outfile:
+                json.dump(results, outfile, sort_keys=True, indent=4, separators=(',', ': '))
+                outfile.write('\n')
