8c7c81813379ad04a01c0436512814667a8b3620
ytesfaye <ytesfaye@iqt.org>
2015-11-11 00:05:12 +0000

    Adding keras to caffe-cuda-cudnn build

---
+++ frameworks/docker/caffe-cuda/caffe-cuda-cudnn-keras/2-run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# image name
+__image=lab41/caffe-cuda-cudnn-keras
+__volume_host=$1
+__volume_cntr=/data
+__ipython_port=$2
+
+# run image
+docker run -it\
+	--device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm \
+        --device /dev/nvidia0:/dev/nvidia0 \
+        --volume=$__volume_host:$__volume_cntr \
+        -p $2:8888 \
+        $__image
