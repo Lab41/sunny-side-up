f1b62cf8af373c4a61b180b61425a9540c585e0e
ytesfaye <ytesfaye@iqt.org>
2015-11-11 00:08:44 +0000

    removing previous keras build which is now integrated into caffe-cuda images

--- frameworks/docker/caffe-theano-keras-cuda/1-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# build base
-__image=lab41/caffe-keras-theano-cuda
-docker build -t $__image .
-
-# __image=lab41/keras-cuda-jupyter
-# docker build -f Dockerfile_jupyter -t $__image .
-
