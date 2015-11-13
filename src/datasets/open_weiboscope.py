a022659d09e14fe8fd2c7e86f12e96633bdbb52c
kylef-lab41 <kylef@lab41.org>
2015-11-13 08:58:12 -0800

    Simplify ingestion of ingesting unicode data from open weiboscope

--- src/datasets/open_weiboscope.py
+++ src/datasets/open_weiboscope.py
@@ -1,43 +1,21 @@
 #!/usr/bin/env python
-
-import os
+import os, sys
 import re
 from itertools import izip_longest
 import random
-import codecs
 import csv
 import logging
 import numpy as np
-#import pandas as pd
-
 from data_utils import get_file, to_one_hot
 
+# update field size
+csv.field_size_limit(sys.maxsize)
 
 # setup logger
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-
-def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
-    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
-    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
-                            dialect=dialect, **kwargs)
-    for row in csv_reader:
-        # decode UTF-8 back to Unicode, cell by cell:
-        yield [unicode(cell, 'utf-8') for cell in row]
-
-def utf_8_encoder(unicode_csv_data): 
-    while True:
-        try:
-            line = unicode_csv_data.next()
-            yield line.encode('utf-8')
-        except UnicodeDecodeError:
-            yield ""
-        except StopIteration:
-            raise StopIteration()
-        except GeneratorExit:
-            break
 
 class BadRecordException(Exception):
     pass
@@ -121,11 +99,14 @@ def load_data(file_path, which_set='train', form='pinyin', train_pct=1.0, nr_rec
     logger.debug(data_sets)
     nr_yielded = 0
     for table_path in data_sets[which_set]:
-        with codecs.open(table_path, "r", encoding="utf-8") as f:
+
+        with open(table_path, 'rb') as f:
+            csv_reader = csv.reader(f, dialect=csv.excel)
+
             logging.debug("In file {}".format(table_path))
-            for line in unicode_csv_reader(f):
+            for line in csv_reader:
                 try:
-                    records_split = line
+                    records_split = [unicode(cell, 'utf-8') for cell in line]
                     post_id = records_split[0]
                     
                     if len(records_split) != 11:
@@ -164,6 +145,8 @@ def load_data(file_path, which_set='train', form='pinyin', train_pct=1.0, nr_rec
                 except BadRecordException as e:
                     logger.debug(e)
                 except IndexError as e:
+                    logger.debug(e)
+                except (UnicodeEncodeError, UnicodeDecodeError) as e:
                     logger.debug(e)
 
                 except GeneratorExit:
