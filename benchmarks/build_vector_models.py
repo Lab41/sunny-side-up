c4c9ceceaffb21df51167ebbde1742d41c25f019
kylef-lab41 <kylef@lab41.org>
2015-11-19 15:07:02 -0800

    Update script to build custom vector models for openweibo and amazon

--- benchmarks/build_vector_models.py
+++ benchmarks/build_vector_models.py
@@ -1,5 +1,10 @@
 from src.datasets.data_utils import WordVectorBuilder
 from src.datasets.open_weiboscope import OpenWeibo
+from src.datasets.amazon_reviews import AmazonReviews
 
-builder = WordVectorBuilder(OpenWeibo, '/data/openweibo/')
-builder.word2vec_save('/data/weibo.bin', min_samples=800000)
+min_samples = 100000
+#builder = WordVectorBuilder(OpenWeibo, '/data/openweibo/')
+#builder.word2vec_save('/data/openweibo/openweibo_{}.bin'.format(min_samples), min_samples=min_samples)
+
+builder = WordVectorBuilder(AmazonReviews, '/data/amazon/amazonreviews.gz')
+builder.word2vec_save('/data/amazon/amazon_{}.bin'.format(min_samples), min_samples=min_samples)
