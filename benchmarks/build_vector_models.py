fd93c6a80cd212dd8d3fe646a7fb73004ab9a06a
kylef-lab41 <kylef@lab41.org>
2015-11-20 14:53:55 -0800

    Add Sentiment140 to custom vector model builder

--- benchmarks/build_vector_models.py
+++ benchmarks/build_vector_models.py
@@ -1,10 +1,7 @@
 from src.datasets.data_utils import WordVectorBuilder
 from src.datasets.open_weiboscope import OpenWeibo
 from src.datasets.amazon_reviews import AmazonReviews
+from src.datasets.sentiment140 import Sentiment140
 
-min_samples = 100000
-#builder = WordVectorBuilder(OpenWeibo, '/data/openweibo/')
-#builder.word2vec_save('/data/openweibo/openweibo_{}.bin'.format(min_samples), min_samples=min_samples)
-
-builder = WordVectorBuilder(AmazonReviews, '/data/amazon/amazonreviews.gz')
-builder.word2vec_save('/data/amazon/amazon_{}.bin'.format(min_samples), min_samples=min_samples)
+builder = WordVectorBuilder(Sentiment140, '/data/sentiment140.csv')
+builder.word2vec_save('/data/sentiment140.bin'.format(min_samples), min_samples=min_samples)
