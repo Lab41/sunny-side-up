0636cac35e951b8ce2cda98df938b5c749c99146
kylef-lab41 <kylef@lab41.org>
2015-11-24 15:45:36 -0800

    Add script to build word2vec model from full open weibo dataset

--- benchmarks/build_vector_models.py
+++ benchmarks/build_vector_models.py
@@ -4,4 +4,4 @@ from src.datasets.amazon_reviews import AmazonReviews
 from src.datasets.sentiment140 import Sentiment140
 
 builder = WordVectorBuilder(Sentiment140, '/data/sentiment140.csv')
-builder.word2vec_save('/data/sentiment140.bin'.format(min_samples), min_samples=min_samples)
+builder.word2vec_save('/data/sentiment140.bin')
