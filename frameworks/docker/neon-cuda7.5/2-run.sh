7e22400405d355d62e3d5da01d7704fbc249fe58
pcallier <pcallier@iqt.org>
2015-11-30 21:04:21 +0000

    Chagne environment variables in Docker runner

--- frameworks/docker/neon-cuda7.5/2-run.sh
+++ frameworks/docker/neon-cuda7.5/2-run.sh
@@ -15,9 +15,7 @@ if [ -e '/dev/nvidia1' ]; then
    CMDSTR="${CMDSTR} --device /dev/nvidia1:/dev/nvidia1 "
 fi
 # mount point, image, and command
-CMDSTR="${CMDSTR} --volume $(pwd):/root/data "
-CMDSTR="${CMDSTR} --volume ${FSERVER}:/root/data/fs4 "
-CMDSTR="${CMDSTR}  '$__image' bash"
-eval "$CMDSTR"
+CMDSTR="${CMDSTR} --volume $(pwd):/root/data '$__image' bash"
 
+eval "$CMDSTR"
 
