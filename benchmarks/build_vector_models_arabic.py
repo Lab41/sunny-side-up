f2a75d0cc696ee7b9afa4b85007a2a3b34d1f26b
kylef-lab41 <kylef@lab41.org>
2016-03-14 10:40:58 -0700

    Update output for building Arabic Tweet vectors

--- benchmarks/build_vector_models_arabic.py
+++ benchmarks/build_vector_models_arabic.py
@@ -6,14 +6,20 @@ import random
 sentences = ArabicTwitterIterator('/data/arabic_tweets/arabic_tweets/all.txt')
 
 # build vocabulary
+print "building vocabulary..."
 model = Word2Vec(size=200, window=5, min_count=10, workers=32)
 model.build_vocab(sentences)
-model.save_word2vec_format('/data/arabic_tweets/arabic_tweets_min10vocab_vocabonly{}.bin'.format(len(model.vocab)))
+
+# save vocabulary model
+print("saving vocabulary-only model ({} terms)...".format(len(model.vocab)))
+model.save_word2vec_format('/data/arabic_tweets/arabic_tweets_NLTK_min10vocab_vocabonly{}.bin'.format(len(model.vocab)))
 
 # train model
+print "training model..."
 sentences = ArabicTwitterIterator('/data/arabic_tweets/arabic_tweets/all.txt')
 model.train(sentences)
 
 # save model
-model.save_word2vec_format('/data/arabic_tweets/arabic_tweets_min10vocab_vocab{}.bin'.format(len(model.vocab)), binary=True)
-model.save_word2vec_format('/data/arabic_tweets/arabic_tweets_min10vocab_vocab{}.nonbinary'.format(len(model.vocab)), binary=False)
+print "saving model..."
+model.save_word2vec_format('/data/arabic_tweets/arabic_tweets_NLTK_min10vocab_vocab{}.bin'.format(len(model.vocab)), binary=True)
+model.save_word2vec_format('/data/arabic_tweets/arabic_tweets_NLTK_min10vocab_vocab{}.nonbinary'.format(len(model.vocab)), binary=False)
